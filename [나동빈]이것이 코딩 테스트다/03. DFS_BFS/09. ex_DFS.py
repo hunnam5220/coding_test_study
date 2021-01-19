81fc42da8cd43a474d1c192943a30931587a7cbb
juuuun_b  <gudwns5220@gmail.com>
2021-01-19 23:55:34 +0900

    write DFS code

--- [나동빈]이것이 코딩 테스트다/03. DFS_BFS/09. ex_DFS.py
+++ [나동빈]이것이 코딩 테스트다/03. DFS_BFS/09. ex_DFS.py
@@ -1,26 +1,34 @@
-def dfs(graph, stack, visited, step):
-    while False not in visited:
-        stack.append(step)
-        visited[step-1] = True
+def dfs(graph, root_node):
+    visited = []
+    stack = [root_node]
 
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
 
+            graph[node].reverse()
+            stack.extend(graph[node])
 
+    return visited
 
 
 graph = [
     [],
-    [2, 3, 7],
+    [2, 7, 3],
     [1, 4, 5],
     [1, 6],
-    [2],
-    [2],
-    [3, 9],
+    [2, 10], [2],
+    [3, 9, 11],
     [1, 8, 9],
-    [7],
-    [6, 7]
+    [7], [7],
+    [4, 12, 13],
+    [6],
+    [10, 14, 15],
+    [10],
+    [12],
+    [12]
 ]
-visited = [False] * len(graph)
-step = 1
-stack = []
 
-dfs(graph=graph, stack=stack, visited=visited, step=step)
+root_node = 1
+print(dfs(graph, root_node))
