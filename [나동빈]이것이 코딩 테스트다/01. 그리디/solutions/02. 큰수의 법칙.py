f6920ae4be1e26d5153d7b9cea19fdbb72f424a1
juuuun_b <31687036+hunnam5220@users.noreply.github.com>
2021-01-19 10:55:54 +0900

    Delete [나동빈]이것이 코딩 테스트다 directory

--- [나동빈]이것이 코딩 테스트다/01. 그리디/solutions/02. 큰수의 법칙.py
+++
@@ -1,9 +0,0 @@
-from sys import stdin
-
-x, y = map(int, stdin.readline().split())
-arr = []
-
-for step in range(x):
-    arr.append(list(map(int, stdin.readline().split())))
-
-print(min(arr[x-1]))
