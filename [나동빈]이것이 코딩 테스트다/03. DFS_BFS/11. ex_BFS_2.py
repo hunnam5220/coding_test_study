efa4956f03ea2bc72179b287fddce56d36093fb8
toogram_JHJ <jhj@toogram.com>
2021-01-20 16:12:43 +0900

    write BFS_ex2, graph data changed

--- [나동빈]이것이 코딩 테스트다/03. DFS_BFS/11. ex_BFS_2.py
+++ [나동빈]이것이 코딩 테스트다/03. DFS_BFS/11. ex_BFS_2.py
@@ -1,2 +1,40 @@
 from collections import deque as qq
 
+
+def bfs(graph, root_node):
+    visited = [root_node]
+
+    for x in range(root_node, len(graph)):
+        queue = qq(graph[x])
+        while queue:
+            n = queue.popleft()
+            if n not in visited:
+                visited.append(n)
+
+            for y in graph[n]:
+                if y not in visited:
+                    queue.append(y)
+                    
+    return visited
+
+
+graph = [
+    [],
+    [2, 7, 3],
+    [1, 4, 5],
+    [1, 6],
+    [2, 10],
+    [2],
+    [3, 9, 11],
+    [1, 8, 9],
+    [7], [7],
+    [4, 12, 13],
+    [6],
+    [10, 14, 15],
+    [10],
+    [12],
+    [12]
+]
+root_node = 1
+
+print(bfs(graph, root_node))
