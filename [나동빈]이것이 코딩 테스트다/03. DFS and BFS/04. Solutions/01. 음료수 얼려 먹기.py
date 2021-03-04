c758ab7d50e1d25c51a3bd93510f409e31150435
juuuun_b  <gudwns5220@gmail.com>
2021-03-04 21:16:21 +0900

    Update 01. 음료수 얼려 먹기.py

--- [나동빈]이것이 코딩 테스트다/03. DFS and BFS/04. Solutions/01. 음료수 얼려 먹기.py
+++ [나동빈]이것이 코딩 테스트다/03. DFS and BFS/04. Solutions/01. 음료수 얼려 먹기.py
@@ -1,16 +1,40 @@
 from sys import stdin
 
 
-def dfs(arr, root_node, visited):
-    pass
-
-
 n, m = map(int, stdin.readline().rstrip().split())
 arr = []
 
 for step in range(n):
-    arr.append(list(stdin.readline().rstrip()))
+    arr.append(list(map(int, stdin.readline().rstrip())))
 
-root_node = 1
-visited = [False] * (n * m)
-dfs(arr, root_node, visited)
+
+def dfs(x, y):
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return False
+
+    if arr[x][y] == 0:
+        arr[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+
+    return False
+
+
+result = 0
+for x in range(n):
+    for y in range(m):
+        if dfs(x, y):
+            result += 1
+
+print(result)
+
+'''
+4 5
+00110
+00011
+11111
+00000
+'''
