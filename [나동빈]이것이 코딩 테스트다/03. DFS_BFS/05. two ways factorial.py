ca68c4d760958b4dc09a3ca15a652173539cd014
juuuun_b  <gudwns5220@gmail.com>
2021-03-04 16:25:59 +0900

    Create 05. two ways factorial.py

---
+++ [나동빈]이것이 코딩 테스트다/03. DFS_BFS/05. two ways factorial.py
@@ -0,0 +1,20 @@
+from sys import stdin
+
+
+def recursive_func(n):
+    if n == 1:
+        return n
+    else:
+        return recursive_func(n - 1) * n
+
+
+def iterative_func(n):
+    result = 1
+    for step in range(1, n+1):
+        result *= step
+    return result
+
+
+n = int(stdin.readline().rstrip())
+print(recursive_func(n))
+print(iterative_func(n))
