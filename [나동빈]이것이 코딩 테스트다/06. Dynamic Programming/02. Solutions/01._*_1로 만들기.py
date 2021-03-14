fb1b885c7443c05838de095f73cf9ba37207dc81
juuuun_b  <gudwns5220@gmail.com>
2021-03-14 13:32:52 +0900

    Create 01._*_1로 만들기.py

---
+++ [나동빈]이것이 코딩 테스트다/06. Dynamic Programming/02. Solutions/01._*_1로 만들기.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+d = [0] * 300001
+n = int(stdin.readline().rstrip())
+
+for i in range(2, n + 1):
+
+    d[i] = d[i - 1] + 1
+
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i // 5] + 1)
+
+
+print(d[n])
+
