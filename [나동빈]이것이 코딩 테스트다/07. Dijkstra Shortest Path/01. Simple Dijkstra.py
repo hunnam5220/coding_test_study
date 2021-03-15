015d6517798dc44ed6057b3ff5bd33451587a3b4
juuuun_b  <gudwns5220@gmail.com>
2021-03-15 15:22:53 +0900

    Create 01. Simple Dijkstra.py

---
+++ [나동빈]이것이 코딩 테스트다/07. Dijkstra Shortest Path/01. Simple Dijkstra.py
@@ -0,0 +1,49 @@
+from sys import stdin
+INF = int(1e9)
+
+n, m = map(int, stdin.readline().split())
+start_node = int(stdin.readline().rstrip())
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, stdin.readline().split())
+    graph[a].append((b, c))
+
+
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+
+    return index
+
+
+def dijkstra(start_node):
+    distance[start_node] = 0
+    visited[start_node] = True
+    for j in graph[start_node]:
+        distance[j[0]] = j[1]
+
+    for i in range(n - 1):
+        now = get_smallest_node()
+        visited[now] = True
+
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+
+dijkstra(start_node)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print('INFINITY')
+    else:
+        print(distance[i])
