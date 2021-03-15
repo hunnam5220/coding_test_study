9ee60b63ad8f08ecc6432803ed74669fd0d82b0e
juuuun_b  <gudwns5220@gmail.com>
2021-03-15 17:16:28 +0900

    Update 01. Simple Dijkstra.py

--- [나동빈]이것이 코딩 테스트다/07. Dijkstra Shortest Path/01. Simple Dijkstra.py
+++ [나동빈]이것이 코딩 테스트다/07. Dijkstra Shortest Path/01. Simple Dijkstra.py
@@ -3,19 +3,19 @@ INF = int(1e9)
 
 n, m = map(int, stdin.readline().split())
 start_node = int(stdin.readline().rstrip())
-graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
-distance = [INF] * (n + 1)
+graph = [[] for _ in range(n)]
+visited = [False] * n
+distance = [INF] * n
 
 for _ in range(m):
     a, b, c = map(int, stdin.readline().split())
-    graph[a].append((b, c))
+    graph[a - 1].append((b - 1, c))
 
 
 def get_smallest_node():
     min_value = INF
     index = 0
-    for i in range(1, n + 1):
+    for i in range(n):
         if distance[i] < min_value and not visited[i]:
             min_value = distance[i]
             index = i
@@ -24,9 +24,9 @@ def get_smallest_node():
 
 
 def dijkstra(start_node):
-    distance[start_node] = 0
-    visited[start_node] = True
-    for j in graph[start_node]:
+    distance[start_node - 1] = 0
+    visited[start_node - 1] = True
+    for j in graph[start_node - 1]:
         distance[j[0]] = j[1]
 
     for i in range(n - 1):
@@ -42,8 +42,8 @@ def dijkstra(start_node):
 
 dijkstra(start_node)
 
-for i in range(1, n + 1):
-    if distance[i] == INF:
+for step in distance:
+    if step == INF:
         print('INFINITY')
     else:
-        print(distance[i])
+        print(step)
