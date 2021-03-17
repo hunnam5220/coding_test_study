3e74b6bd729dc4f79b90ecac5be5a82c264c8708
juuuun_b  <gudwns5220@gmail.com>
2021-03-18 00:17:45 +0900

    Update 01. Floyd-Warshall.py

--- [나동빈]이것이 코딩 테스트다/07. Shortest Path/02. All to All/01. Floyd-Warshall/01. Floyd-Warshall.py
+++ [나동빈]이것이 코딩 테스트다/07. Shortest Path/02. All to All/01. Floyd-Warshall/01. Floyd-Warshall.py
@@ -1,16 +1,21 @@
 from sys import stdin
 
+""" 무한을 뜻하는 INF 변수에 10억을 저장 """
 INF = int(1e9)
 
+""" 노드의 개수 및 간선의 개수 입력 받기 """
 n = int(stdin.readline().rstrip())
 m = int(stdin.readline().rstrip())
 
+""" n * n (노드 개수 제곱)만큼 빙고판 만들기 """
 graph = [[INF] * n for _ in range(n)]
 
+""" 출발 노드와 도착 노드가 같으면 0을 입력 """
 for a in range(n):
     for b in range(n):
         if a == b:
             graph[a][b] = 0
+
 
 for _ in range(m):
     a, b, c = map(int, stdin.readline().split())
