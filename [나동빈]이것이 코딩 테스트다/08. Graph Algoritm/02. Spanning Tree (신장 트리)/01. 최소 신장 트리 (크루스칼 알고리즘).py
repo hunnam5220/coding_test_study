863e6e32756c773fe3b0de3f71c0f64b9bacf763
juuuun_b  <gudwns5220@gmail.com>
2021-03-20 00:52:56 +0900

    Update 01. 최소 신장 트리 (크루스칼 알고리즘).py

--- [나동빈]이것이 코딩 테스트다/08. Graph Algoritm/02. Spanning Tree (신장 트리)/01. 최소 신장 트리 (크루스칼 알고리즘).py
+++ [나동빈]이것이 코딩 테스트다/08. Graph Algoritm/02. Spanning Tree (신장 트리)/01. 최소 신장 트리 (크루스칼 알고리즘).py
@@ -1,2 +1,52 @@
 from sys import stdin
 
+
+def find_parent(parent, x):
+    if parent[x - 1] != x:
+        parent[x - 1] = find_parent(parent, parent[x - 1])
+    return parent[x - 1]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[a - 1] = b
+    else:
+        parent[b - 1] = a
+
+
+v, e = map(int, stdin.readline().split())
+parent = [step for step in range(1, v + 1)]
+
+edges = []
+result = 0
+
+for step in range(e):
+    a, b, cost = map(int, stdin.readline().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
+
+""" 
+7 9
+1 2 29
+1 5 75
+2 3 35
+2 6 34
+3 4 7
+4 6 23
+4 7 13
+5 6 53
+6 7 25
+"""
