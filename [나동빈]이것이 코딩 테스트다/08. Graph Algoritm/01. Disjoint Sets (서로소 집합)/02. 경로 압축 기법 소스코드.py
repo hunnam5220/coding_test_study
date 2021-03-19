678586c9f502ffa9957d9600b3910a29eda78e05
juuuun_b  <gudwns5220@gmail.com>
2021-03-19 22:15:54 +0900

    Update 02. 경로 압축 기법 소스코드.py

--- [나동빈]이것이 코딩 테스트다/08. Graph Algoritm/01. Disjoint Sets (서로소 집합)/02. 경로 압축 기법 소스코드.py
+++ [나동빈]이것이 코딩 테스트다/08. Graph Algoritm/01. Disjoint Sets (서로소 집합)/02. 경로 압축 기법 소스코드.py
@@ -1,4 +1,4 @@
 def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+    if parent[x - 1] != x:
+        parent[x - 1] = find_parent(parent, parent[x - 1])
+    return parent[x - 1]
