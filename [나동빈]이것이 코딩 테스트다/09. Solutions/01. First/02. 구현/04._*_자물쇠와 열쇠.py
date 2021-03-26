27d1b7f4da6d9d5797969fef16ef3566818d9d9d
juuuun_b  <gudwns5220@gmail.com>
2021-03-26 18:51:07 +0900

    Update 04._*_자물쇠와 열쇠.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/02. 구현/04._*_자물쇠와 열쇠.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/02. 구현/04._*_자물쇠와 열쇠.py
@@ -1,24 +1,38 @@
 def solution(key, lock):
+    """ Main Function """
+
+    """  """
     n = len(lock)
+    """  """
     m = len(key)
+
+    """ 자물솨 크기의 세 배의 새 자물쇠 생성 """
     gear_third_lock = [[0] * (n * 3) for _ in range(n * 3)]
 
+    """ 새로운 자물쇠 중간에 기존 자물쇠 집어 넣기"""
     for x in range(n):
         for y in range(n):
             gear_third_lock[x + n][y + n] = lock[x][y]
 
+    """ 90도씩 4번 회전 """
     for step in range(4):
+        """ 90도 회전 """
         key = rotate_a_matrix_by_90_degree(key)
 
+        """ 키우는건 세 배, 이동할 수 있는 거리는 3 - 1배 """
         for x in range(n * 2):
             for y in range(n * 2):
+
+                """ 열쇠를 자물쇠 부분에 더함 """
                 for i in range(m):
                     for j in range(m):
                         gear_third_lock[x + i][y + j] += key[i][j]
 
+                """ 자물쇠 부분이 모두 1인지 검사 """
                 if check(gear_third_lock):
                     return True
 
+                """ True return 안됐으니까 다시 원상복구 함 """
                 for i in range(m):
                     for j in range(m):
                         gear_third_lock[x + i][y + j] -= key[i][j]
@@ -50,9 +64,7 @@ def check(lock):
 
 
 key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
-
 lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-
 
 if solution(key, lock):
     print('true')
