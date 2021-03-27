0e57ed83de528560a678e17cd28d09d86dd5fa1e
juuuun_b  <gudwns5220@gmail.com>
2021-03-27 16:57:45 +0900

    Update 05. 뱀.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/02. 구현/05. 뱀.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/02. 구현/05. 뱀.py
@@ -1,91 +1,71 @@
 from sys import stdin
+from collections import deque
+q = deque()
+q.append([0, 0])
 
 
-def dfs(k, j, idx):
-    if k >= n or j >= n or k < 0 or j < 0:
+def move_snake(i, j):
+    if i >= n or j >= n or i < 0 or j < 0 or arr[i][j] == 9:
         return False
 
-    if arr[k][j] == 9:
-        return False
-
-    if arr[k][j] != 0:
-        if check_apple(k, j):
-            if idx == 0:
-                arr[k][j - 1] = 9
-
-            elif idx == 1:
-                arr[k - 1][j] = 9
-
-            elif idx == 2:
-                arr[k + 1][j] = 9
-
-            else:
-                arr[k][j + 1] = 9
-        else:
-            if idx == 0:
-                arr[k][j - 1] = 0
-
-            elif idx == 1:
-                arr[k - 1][j] = 0
-
-            elif idx == 2:
-                arr[k + 1][j] = 0
-
-            else:
-                arr[k][j + 1] = 0
-
-        arr[k][j] = 9
-
+    if arr[i][j] != 0:
+        q.append([i, j])
+        arr[i][j] = 9
         return True
 
-    return True
-
-
-def check_apple(k, j):
-    if arr[k][j] == 1:
+    elif arr[i][j] == 0:
+        a, b = q.popleft()
+        arr[i][j] = 9
+        q.append([i, j])
+        arr[a][b] = 0
         return True
-    return False
 
 
 n = int(stdin.readline().rstrip())
 arr = [[0] * n for _ in range(n)]
-global k, j
-k, j = 0, 0
-c, chk = '', True
-time, idx = 0, 0
+arr[0][0] = 9
+time, idx, chk = 0, 0, True
+i, j = 0, 0
 
 for step in range(int(stdin.readline().rstrip())):
     a, b = map(int, stdin.readline().split())
     arr[a - 1][b - 1] = 1
-
-for step in range(int(stdin.readline().rstrip())):
+l = int(stdin.readline().rstrip())
+for step in range(l):
     x, c = stdin.readline().split()
-    for k in range(int(x)):
-        if chk:
+
+    if chk:
+        while 1:
+            time += 1
             if idx == 0:
                 j += 1
+
             elif idx == 1:
-                k += 1
+                i += 1
+
             elif idx == 2:
-                k -= 1
-            else:
                 j -= 1
 
-            time += 1
-            chk = dfs(k, j, idx)
-            if not chk:
+            else:
+                i -= 1
+
+            if not move_snake(i, j):
                 print(time)
+                chk = False
                 break
 
-    if c == 'D':
-        idx += 1
-        if idx >= 4:
-            idx = 0
-    else:
-        idx -= 1
-        if idx <= -1:
-            idx = 3
+            if time == int(x):
+                if c == 'D':
+                    idx += 1
+                    if idx >= 4:
+                        idx = 0
+                else:
+                    idx -= 1
+                    if idx <= -1:
+                        idx = 3
 
+                if step != l - 1:
+                    break
 
 
 
