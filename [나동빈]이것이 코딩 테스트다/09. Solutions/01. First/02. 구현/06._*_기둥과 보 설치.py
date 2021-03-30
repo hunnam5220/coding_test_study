b03f32cb4fbc483e69c70f9f7e1d4134b012e98e
juuuun_b  <gudwns5220@gmail.com>
2021-03-30 16:17:39 +0900

    Update 06._*_기둥과 보 설치.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/02. 구현/06._*_기둥과 보 설치.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/02. 구현/06._*_기둥과 보 설치.py
@@ -1,5 +1,16 @@
 def chk(answer):
+    for x, y, stuff in answer:
+        if stuff == 0:
+            if y == 0 or [x - 1, y, 1] in answer or [x, y - 1, 0] in answer or [x, y, 1] in answer:
+                continue
+            return False
 
+        elif stuff == 1:
+            if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
+                continue
+            return False
+
+    return True
 
 
 def solution(n, build_frame):
@@ -8,22 +19,25 @@ def solution(n, build_frame):
         x, y, stuff, oper = frame
         if oper == 0:
             answer.remove([x, y, stuff])
-            if not chk()
+            if not chk(answer):
+                answer.append([x, y, stuff])
 
         else:
             answer.append([x, y, stuff])
             if not chk(answer):
                answer.remove([x, y, stuff])
 
+    return sorted(answer)
 
 
 
-build_frame = [[1, 0, 0, 1], [1, 1, 1, 1], [2, 1, 0, 1], [2, 2, 1, 1], [5, 0, 0, 1], [5, 1, 0, 1], [4, 2, 1, 1], [3, 2, 1, 1]]
-# [[1,0,0],[1,1,1],[2,1,0],[2,2,1],[3,2,1],[4,2,1],[5,0,0],[5,1,0]]
-
-# build_frame = [[0, 0, 0, 1], [2, 0, 0, 1], [4, 0, 0, 1], [0, 1, 1, 1], [1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1],
-# [2, 0, 0, 0], [1, 1, 1, 0], [2, 2, 0, 1]]
-# [[0,0,0],[0,1,1],[1,1,1],[2,1,1],[3,1,1],[4,0,0]]
 
 n = 5
+# build_frame = [[1, 0, 0, 1], [1, 1, 1, 1], [2, 1, 0, 1], [2, 2, 1, 1], [5, 0, 0, 1], [5, 1, 0, 1], [4, 2, 1, 1], [3, 2, 1, 1]]
+# [[1,0,0],[1,1,1],[2,1,0],[2,2,1],[3,2,1],[4,2,1],[5,0,0],[5,1,0]]
+# print(solution(n, build_frame))
+
+build_frame = [[0, 0, 0, 1], [2, 0, 0, 1], [4, 0, 0, 1], [0, 1, 1, 1], [1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1],
+[2, 0, 0, 0], [1, 1, 1, 0], [2, 2, 0, 1]]
+# [[0,0,0],[0,1,1],[1,1,1],[2,1,1],[3,1,1],[4,0,0]]
 print(solution(n, build_frame))
