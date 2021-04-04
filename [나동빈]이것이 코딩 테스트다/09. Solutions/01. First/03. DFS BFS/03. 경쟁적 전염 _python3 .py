95485f745b24407b1ce4010e9e9876ea0c6e9485
juuuun_b  <gudwns5220@gmail.com>
2021-04-04 16:00:14 +0900

    Update 03. 경쟁적 전염 _python3 .py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/03. DFS BFS/03. 경쟁적 전염 _python3 .py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/03. DFS BFS/03. 경쟁적 전염 _python3 .py
@@ -1,38 +1,38 @@
 # BFS로 풀어보기
-
+from collections import deque
 from sys import stdin
 
 n, k = map(int, stdin.readline().split())
 graph = []
 # 데이터 리스트 생성
+data = []
 
+for i in range(n):
+    graph.append(list(map(int, stdin.readline().split())))
+    # 바이러스 존재하면 시간 정보, 바이러스 좌표 삽입
+    for q in range(n):
+        if graph[i][q] != 0:
+            data.append([graph[i][q], 0, i, q])
 
-for step in range(n):
-    pp = list(map(int, stdin.readline().split()))
-    # 바이러스 존재하면 바이러스 좌표, 시간 정보 삽입
+data.sort()
+q = deque(data)
 
-tmp = [item[:] for item in graph]
-
-s, x, y = map(int, stdin.readline().split())
+ts, tx, ty = map(int, stdin.readline().split())
 dx = [0, -1, 0, 1]
 dy = [-1, 0, 1, 0]
 
+while q:
+    v, s, x, y = q.popleft()
+    if s == ts:
+        break
 
-def infection(number, nx, ny):
-    for i in range(4):
-        kx, ky = nx + dx[i], ny + dy[i]
+    for step in range(4):
+        nx = x + dx[step]
+        ny = y + dy[step]
 
-        if (n > kx > -1 and n > ky > -1) and tmp[kx][ky] == 0:
-            tmp[kx][ky] = number
+        if 0 <= nx < n and 0 <= ny < n:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = v
+                q.append([v, s + 1, nx, ny])
 
-
-for step in range(s):
-    for number in range(1, k + 1):
-        for nx in range(n):
-            for ny in range(n):
-                if graph[nx][ny] == number:
-                    infection(number, nx, ny)
-
-    graph = [item[:] for item in tmp]
-
-print(tmp[x - 1][y - 1])
+print(graph[tx - 1][ty - 1])
