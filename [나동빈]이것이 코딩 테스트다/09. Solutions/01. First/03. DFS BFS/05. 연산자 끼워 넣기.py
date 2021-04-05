7a95f3f5201f7a89615ac007bb5efdc92dc7dbaf
juuuun_b  <gudwns5220@gmail.com>
2021-04-05 22:45:14 +0900

    Update 05. 연산자 끼워 넣기.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/03. DFS BFS/05. 연산자 끼워 넣기.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/03. DFS BFS/05. 연산자 끼워 넣기.py
@@ -1,10 +1,26 @@
 from sys import stdin
-from itertools import product
+from itertools import combinations
+from collections import deque
 
 n = int(stdin.readline().rstrip())
-arr = list(map(int, stdin.readline().split()))
-a, b, c, d = map(int, stdin.readline().split())
-cal = [list('+' * a), list('-' * b), list('*' * c), list('/' * d)]
+arr = list(stdin.readline().rstrip().split())
+cal = []
+d = {0: '+', 1: '-', 2: '*', 3: '/'}
+result = []
 
-# 모든 조합 구하기 구현
+for x, i in zip(list(map(int, stdin.readline().split())), range(4)):
+    for _ in range(x):
+        cal.append(d[i])
 
+"""
+arr 길이 - 1  >> cal 길이
+
+arr 사이에 cal의 조합을 넣어서 계산하고
+
+계산값이 있는 것들의 맥스 민 값을 출력
+
+"""
+
+
+print(max(result))
+print(min(result))
