771521a260bbceb2b2ee8ad17ebbf9a94d7fd2b2
juuuun_b  <gudwns5220@gmail.com>
2021-04-06 00:18:54 +0900

    Update 05. 연산자 끼워 넣기.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/03. DFS BFS/05. 연산자 끼워 넣기.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/03. DFS BFS/05. 연산자 끼워 넣기.py
@@ -1,26 +1,79 @@
 from sys import stdin
-from itertools import combinations
+from itertools import permutations
 from collections import deque
 
 n = int(stdin.readline().rstrip())
 arr = list(stdin.readline().rstrip().split())
+arr.sort()
 cal = []
 d = {0: '+', 1: '-', 2: '*', 3: '/'}
 result = []
+
+
+def get_cal_string(prod):
+    cal_string = ''
+    leng = len(arr)
+    for s in range(leng):
+        cal_string += arr[s]
+        if s < (leng - 1):
+            cal_string += prod[s]
+
+    return cal_string
+
+
+def get_calc_val(cal_st):
+    q = deque(list(cal_st))
+    val, tmp, code = None, None, None
+    while q:
+        s = q.popleft()
+
+        if s.isdigit() and val is None:
+            val = int(s)
+
+        elif not s.isdigit():
+            code = s
+
+        elif s.isdigit() and val is not None:
+            tmp = int(s)
+
+        if val is not None and tmp is not None and code is not None:
+            if code == '+':
+                val += tmp
+            elif code == '-':
+                val -= tmp
+            elif code == '*':
+                val *= tmp
+            elif code == '/':
+                if val < 0 and tmp > 0:
+                    val = abs(val) // tmp * -1
+                elif tmp == 0:
+                    val = 0
+                else:
+                    val //= tmp
+
+            tmp, code = None, None
+
+    return val
+
 
 for x, i in zip(list(map(int, stdin.readline().split())), range(4)):
     for _ in range(x):
         cal.append(d[i])
 
-"""
-arr 길이 - 1  >> cal 길이
+cal_prod = list(permutations(cal, len(cal)))
 
-arr 사이에 cal의 조합을 넣어서 계산하고
+max_val = 0
+min_val = int(1e9)
 
-계산값이 있는 것들의 맥스 민 값을 출력
+for step in range(len(cal_prod)):
+    calc_string = get_cal_string(cal_prod[step])
+    val = get_calc_val(calc_string)
 
-"""
+    if val < min_val:
+        min_val = val
 
+    if val > max_val:
+        max_val = val
 
-print(max(result))
-print(min(result))
+print(max_val)
+print(min_val)
