8a192696e1282509bd30ce8b2f8855311709510b
juuuun_b  <gudwns5220@gmail.com>
2021-04-15 18:46:36 +0900

    Update 04._*_병사 배치하기.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/06. 다이나믹 프로그래밍/04._*_병사 배치하기.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/01. First/06. 다이나믹 프로그래밍/04._*_병사 배치하기.py
@@ -8,7 +8,7 @@ dp = [1] * n
 
 # 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
 for i in range(1, n):
-    for j in range(0, i):
+    for j in range(i):
         if array[j] < array[i]:
             dp[i] = max(dp[i], dp[j] + 1)
 
