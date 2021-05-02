f36b41c6f1a998c1d5cfc838ff9d4341ee57de04
juuuun_b  <gudwns5220@gmail.com>
2021-05-02 19:16:30 +0900

    Update 03. 문자열 압축.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/04. Fourth/02. 구현/03. 문자열 압축.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/04. Fourth/02. 구현/03. 문자열 압축.py
@@ -1,4 +1,37 @@
 def solution(s):
-    answer = 0
+    length = len(s)
+    answer = 1e9
+    for k in range(1, length + 1):
+        check = s[0:k]
+        temp_str = s[0:k]
+        cnt = 1
+
+        for j in range(k, length + 1, k):
+            if s[j:j + k] == check:
+                cnt += 1
+            else:
+                if cnt > 1:
+                    temp_str += str(cnt)
+                    cnt = 1
+                temp_str += s[j:j + k]
+                check = s[j:j + k]
+
+        answer = min(answer, len(temp_str))
+
     return answer
 
+
+# 7
+print(solution("aabbaccc"))
+
+# 9
+print(solution("ababcdcdababcdcd"))
+
+# 8
+print(solution("abcabcdede"))
+
+# 14
+print(solution("abcabcabcabcdededededede"))
+
+# 17
+print(solution("xababcdcdababcdcd"))
