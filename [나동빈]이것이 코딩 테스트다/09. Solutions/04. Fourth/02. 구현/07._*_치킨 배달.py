b67abc33d38d250c1b352949dd68e1b24ab4df1c
juuuun_b  <gudwns5220@gmail.com>
2021-05-02 23:00:49 +0900

    Create 07._*_치킨 배달.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/04. Fourth/02. 구현/07._*_치킨 배달.py
@@ -0,0 +1,32 @@
+from sys import stdin
+from itertools import combinations
+
+n, m = map(int, stdin.readline().split())
+chicken_xy = []
+house_xy = []
+
+for x in range(n):
+    data = list(map(int, stdin.readline().split()))
+    for y in range(len(data)):
+        if data[y] == 1:
+            house_xy.append([x, y])
+
+        elif data[y] == 2:
+            chicken_xy.append([x, y])
+
+result = 1e9
+
+for chicken in list(combinations(chicken_xy, m)):
+    tmp_ans = 0
+    for house in house_xy:
+        x1, y1 = house
+        temp_val = 1e9
+        for cc in chicken:
+            x2, y2 = cc
+            temp_val = min(temp_val, abs(x1 - x2) + abs(y1 - y2))
+
+        tmp_ans += temp_val
+
+    result = min(tmp_ans, result)
+
+print(result)
