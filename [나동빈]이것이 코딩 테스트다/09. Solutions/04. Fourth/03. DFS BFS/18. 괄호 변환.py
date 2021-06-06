7a5f1197c240778d9f537b22c4bffbc9f4b84f45
juuuun_b  <gudwns5220@gmail.com>
2021-06-06 11:14:01 +0900

    Create 18. 괄호 변환.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/04. Fourth/03. DFS BFS/18. 괄호 변환.py
@@ -0,0 +1,16 @@
+def get_index(p):
+    pass
+
+
+def check_str():
+    pass
+
+
+def solution(p):
+    answer = ''
+    return answer
+
+
+print(solution("(()())()"))
+print(solution(")("))
+print(solution("()))((()"))
