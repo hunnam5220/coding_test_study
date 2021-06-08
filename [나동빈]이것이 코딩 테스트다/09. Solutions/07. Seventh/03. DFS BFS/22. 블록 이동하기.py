261760363124dfedb50860ea99804b95279eca79
juuuun_b  <gudwns5220@gmail.com>
2021-06-08 14:49:37 +0900

    Create 22. 블록 이동하기.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/07. Seventh/03. DFS BFS/22. 블록 이동하기.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+dx, dy = [1, 0, 0, -1], [0, 1, -1, 0]
+
+
+def get_next_pos(pos, board):
+    pos = list(pos)
+    next_pos = []
+    x1, y1, x2, y2 = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
+
+    for i in range(4):
+        nx1, ny1, nx2, ny2 = x1 + dx[i], y1 + dy[i], x2 + dx[i], y2 + dy[i]
+
+        if board[nx1][ny1] == 0 and board[nx2][ny2] == 0:
+            next_pos.append({(nx1, ny1), (nx2, ny2)})
+
+    if x1 == x2:
+        for i in [-1, 1]:
+            if board[x1 + i][y1] == 0 and board[x2 + i][y2] == 0:
+                next_pos.append({(x1, y1), (x1 + i, y1)})
+                next_pos.append({(x2, y2), (x2 + i, y2)})
+
+    if y1 == y2:
+        for i in [-1, 1]:
+            if board[x1][y1 + i] == 0 and board[x2][y2 + i] == 0:
+                next_pos.append({(x1, y1), (x1, y1 + i)})
+                next_pos.append({(x2, y2), (x2, y2 + i)})
+
+    return next_pos
+
+
+def solution(board):
+    n = len(board)
+
+    new_board = [[1] * (n + 2) for _ in range(n + 2)]
+
+    for i in range(n):
+        for j in range(n):
+            new_board[i + 1][j + 1] = board[i][j]
+
+    pos = {(1, 1), (1, 2)}
+    q = deque()
+    visited = [pos]
+    cost = 0
+    q.append((pos, cost))
+
+    while q:
+        pos, cost = q.popleft()
+        if (n, n) in pos:
+            return cost
+        for next_pos in get_next_pos(pos, new_board):
+            if next_pos not in visited:
+                q.append((next_pos, cost + 1))
+                visited.append(next_pos)
+
+
+board = [[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]
+print(solution(board))
