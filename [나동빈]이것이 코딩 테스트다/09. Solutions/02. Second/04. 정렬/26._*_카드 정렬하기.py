9de5ab6bf6618e38756445b3a48a074cd87b4823
juuuun_b  <gudwns5220@gmail.com>
2021-06-08 18:39:35 +0900

    Update 26._*_카드 정렬하기.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/02. Second/04. 정렬/26._*_카드 정렬하기.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/02. Second/04. 정렬/26._*_카드 정렬하기.py
@@ -15,7 +15,7 @@ while len(heap) != 1:
     # 가장 작은 2개의 카드 묶음 꺼내기
     one = heapq.heappop(heap)
     two = heapq.heappop(heap)
-    
+
     # 카드 묶음을 합쳐서 다시 삽입
     sum_value = one + two
     result += sum_value
