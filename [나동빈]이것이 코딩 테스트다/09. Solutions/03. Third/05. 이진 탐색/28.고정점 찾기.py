3c0f49395e8fc2ab4f673606cb9f3dfd37a3dc04
juuuun_b  <gudwns5220@gmail.com>
2021-06-09 16:42:48 +0900

    Update 28.고정점 찾기.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/03. Third/05. 이진 탐색/28.고정점 찾기.py
@@ -0,0 +1,45 @@
+from sys import stdin
+
+
+def binary_search(arr, start, end):
+    if start > end:
+        return
+
+    mid = (start + end) // 2
+
+    if arr[mid] == mid:
+        return mid
+
+    elif arr[mid] < mid:
+        return binary_search(arr, mid + 1, end)
+    else:
+        return binary_search(arr, start, mid - 1)
+
+
+n = int(stdin.readline())
+arr = list(map(int, stdin.readline().split()))
+idx = binary_search(arr, 0, n - 1)
+
+
+if idx is None:
+    print(-1)
+else:
+    print(idx)
+
+# 3
+"""
+5
+-15 -6 1 3 7
+"""
+
+# 2
+"""
+7
+-15 -4 2 8 9 13 15
+"""
+
+# -1
+"""
+7
+-15 -4 3 8 9 13 15
+"""
