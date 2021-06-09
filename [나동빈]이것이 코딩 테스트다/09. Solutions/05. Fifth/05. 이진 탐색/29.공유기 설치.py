8e8281d96af14ba9d0054f46349d74a885087e96
juuuun_b  <gudwns5220@gmail.com>
2021-06-09 19:01:57 +0900

    Update 29.공유기 설치.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/05. Fifth/05. 이진 탐색/29.공유기 설치.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+
+def solution(n, c, arr):
+    arr.sort()
+
+    start = 0
+    end = arr[-1]
+    res = 0
+
+    while start <= end:
+        val = arr[0]
+        mid = (start + end) // 2
+        cnt = 1
+
+        for i in range(n):
+            if arr[i] >= val + mid:
+                val = arr[i]
+                cnt += 1
+
+            start = mid + 1
+            res = mid
+        else:
+            end = mid - 1
+
+    return res
+
+
+n, c = map(int, stdin.readline().split())
+print(solution(n, c, [int(stdin.readline()) for _ in range(n)]))
