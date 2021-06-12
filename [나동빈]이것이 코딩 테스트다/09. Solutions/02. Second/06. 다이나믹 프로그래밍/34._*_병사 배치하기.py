fdbdb97fdc9c8085f8519bb52077d8fb88dd1a7b
juuuun_b  <gudwns5220@gmail.com>
2021-06-12 19:26:18 +0900

    Create 34._*_병사 배치하기.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/02. Second/06. 다이나믹 프로그래밍/34._*_병사 배치하기.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+n = int(stdin.readline())
+
+arr = list(map(int, stdin.readline().split()))
+arr.reverse()
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
