d3a9a7d14323e17a33c011c6078800b6e9e83531
juuuun_b  <gudwns5220@gmail.com>
2021-06-18 14:51:30 +0900

    Create 38._*_정확한 순위.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/04. Fourth/07. 최단 거리 탐색/38._*_정확한 순위.py
@@ -0,0 +1,49 @@
+from sys import stdin
+
+
+def solution():
+    n, m = map(int, stdin.readline().split())
+    inf = int(1e9)
+    arr = [[inf] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i == j:
+                arr[i][i] = 0
+
+    for _ in range(m):
+        a, b = map(int, stdin.readline().split())
+        arr[a][b] = 1
+
+    res = 0
+
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j])
+
+    for i in range(1, n + 1):
+        cnt = 0
+
+        for j in range(1, n + 1):
+            if arr[i][j] != inf or arr[j][i] != inf:
+                cnt += 1
+
+        if cnt == n:
+            res += 1
+
+    return res
+
+
+print(solution())
+
+
+"""
+6 6
+1 5
+3 4
+4 2
+4 6
+5 2
+5 4
+"""
