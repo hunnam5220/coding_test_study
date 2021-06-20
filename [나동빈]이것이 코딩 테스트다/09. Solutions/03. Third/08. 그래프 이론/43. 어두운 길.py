27d4f6e219be79742ad6c8ab969bd5b288b9090c
juuuun_b  <gudwns5220@gmail.com>
2021-06-20 17:41:32 +0900

    Update 43. 어두운 길.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/03. Third/08. 그래프 이론/43. 어두운 길.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/03. Third/08. 그래프 이론/43. 어두운 길.py
@@ -1,11 +1,49 @@
 from sys import stdin
 
 
-def solution():
-    return
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 
-print(solution())
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+def solution(n, m):
+    parent = [0] * (n + 1)
+    for i in range(1, n + 1):
+        parent[i] = i
+
+    data = []
+    for _ in range(m):
+        a, b, c = map(int, stdin.readline().split())
+        data.append((c, a, b))
+
+    data.sort()
+
+    res = 0
+    total = 0
+
+    for e in data:
+        cost, a, b = e
+        total += cost
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            res += cost
+
+    return total - res
+
+
+N, M = map(int, stdin.readline().split())
+print(solution(N, M))
 
 """
 7 11
