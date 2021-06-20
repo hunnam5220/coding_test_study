b59727200ba96a5c7265ceef5e5b67f7c43fc5c2
juuuun_b  <gudwns5220@gmail.com>
2021-06-20 23:55:37 +0900

    Create 45._*_최종 순위.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/03. Third/08. 그래프 이론/45._*_최종 순위.py
@@ -0,0 +1,107 @@
+from sys import stdin
+from collections import deque
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+def solution(n):
+    grade = list(map(int, stdin.readline().split()))
+
+    graph = [[False] * (n + 1) for _ in range(n + 1)]
+    indegree = [0] * (n + 1)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            graph[grade[i]][grade[j]] = True
+            indegree[grade[j]] += 1
+
+    m = int(stdin.readline())
+
+    for _ in range(m):
+        a, b = map(int, stdin.readline().split())
+
+        if graph[a][b]:
+            graph[a][b] = False
+            graph[b][a] = True
+            indegree[a] += 1
+            indegree[b] -= 1
+
+        else:
+            graph[a][b] = True
+            graph[b][a] = False
+            indegree[a] -= 1
+            indegree[b] += 1
+
+    q = deque()
+    res = []
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    certain = True
+    cycle = False
+
+    for i in range(n):
+        if len(q) == 0:
+            cycle = True
+            break
+
+        if len(q) >= 2:
+            certain = False
+
+        now = q.popleft()
+        res.append(now)
+
+        for j in range(1, n + 1):
+            if graph[now][j]:
+                indegree[j] -= 1
+                if indegree[j] == 0:
+                    q.append(j)
+
+    if cycle:
+        print("IMPOSSIBLE")
+
+    elif not certain:
+        print("?")
+
+    else:
+        for i in res:
+            print(i, end=' ')
+
+        print()
+
+
+for tc in range(int(stdin.readline())):
+    solution(int(stdin.readline()))
+
+"""
+3
+5
+5 4 3 2 1
+2
+2 4
+3 4
+3
+2 3 1
+0
+4
+1 2 3 4
+3
+1 2
+3 4
+2 3
+"""
