23b5f45a74769d2971f9d54591052983b10dd363
juuuun_b  <gudwns5220@gmail.com>
2021-06-20 17:13:08 +0900

    Create 42._*_탑승구.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/03. Third/08. 그래프 이론/42._*_탑승구.py
@@ -0,0 +1,47 @@
+from sys import stdin
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+def solution(g, p):
+    parent = [0] * (g + 1)
+    for i in range(1, g + 1):
+        parent[i] = i
+
+    res = 0
+
+    for _ in range(p):
+        data = find_parent(parent, int(stdin.readline()))
+        if data == 0:
+            break
+
+        union_parent(parent, data, data - 1)
+        res += 1
+
+    return res
+
+
+G, P = int(stdin.readline()), int(stdin.readline())
+print(solution(G, P))
+
+"""
+4
+3
+4
+1
+1
+"""
