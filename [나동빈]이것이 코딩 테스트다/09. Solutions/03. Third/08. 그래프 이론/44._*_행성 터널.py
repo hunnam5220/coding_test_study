f58e8aa3f62853cd88ad1f91282acc675c5fa1d7
juuuun_b  <gudwns5220@gmail.com>
2021-06-20 18:06:22 +0900

    Create 44._*_행성 터널.py

---
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/03. Third/08. 그래프 이론/44._*_행성 터널.py
@@ -0,0 +1,67 @@
+from sys import stdin
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+def solution(n):
+    parent = [0] * (n + 1)
+    for i in range(1, n):
+        parent[i] = i
+
+    edges = []
+    result = 0
+
+    x, y, z = [], [], []
+
+    for i in range(1, n + 1):
+        a, b, c = map(int, stdin.readline().split())
+        x.append((a, i))
+        y.append((b, i))
+        z.append((c, i))
+
+    x.sort()
+    y.sort()
+    z.sort()
+
+    for i in range(n - 1):
+        edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+        edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+        edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
+
+    edges.sort()
+    res = 0
+
+    for edge in edges:
+        cost, a, b = edge
+
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            res += cost
+
+    return res
+
+
+print(solution(int(stdin.readline())))
+
+"""
+5
+11 -15 -15
+14 -5 -15
+-1 -1 -5
+10 -4 -1
+19 -4 19
+"""
