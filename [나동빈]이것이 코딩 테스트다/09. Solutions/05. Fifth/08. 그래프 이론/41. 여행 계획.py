ae744e7652ba4a1fa506565d3cce4eac025d326c
juuuun_b  <gudwns5220@gmail.com>
2021-06-22 17:48:47 +0900

    Update 41. 여행 계획.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/05. Fifth/08. 그래프 이론/41. 여행 계획.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/05. Fifth/08. 그래프 이론/41. 여행 계획.py
@@ -1,11 +1,46 @@
 from sys import stdin
 
 
-def solution():
-    return
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 
-print(solution())
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+def solution(n, m):
+    parent = [0] * (n + 1)
+    for i in range(1, n + 1):
+        parent[i] = i
+
+    for i in range(1, n + 1):
+        data = list(map(int, stdin.readline().split()))
+        for j in range(n):
+            if data[j] == 1:
+                union_parent(parent, i, j + 1)
+
+    plan = list(map(int, stdin.readline().split()))
+
+    res = True
+
+    for i in range(m - 1):
+        if find_parent(parent, plan[i]) != find_parent(parent, plan[i + 1]):
+            res = False
+
+    return "YES" if res else "NO"
+
+
+N, M = map(int, stdin.readline().split())
+print(solution(N, M))
 
 """
 5 4
