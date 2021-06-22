1d389ea9ecb8372a7ed5ca857946411144a437a3
juuuun_b  <gudwns5220@gmail.com>
2021-06-22 17:52:13 +0900

    Update 42. 탑승구.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/05. Fifth/08. 그래프 이론/42. 탑승구.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/05. Fifth/08. 그래프 이론/42. 탑승구.py
@@ -1,11 +1,30 @@
 from sys import stdin
 
 
-def solution():
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+def solution(g, p):
+    parent = [0] * (g + 1)
+    
     return
 
 
-print(solution())
+G, P = map(int, stdin.readline().split())
+print(solution(G, P))
 
 """
 4
