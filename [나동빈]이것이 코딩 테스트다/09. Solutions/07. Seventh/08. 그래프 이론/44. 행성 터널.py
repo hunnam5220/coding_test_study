fe0b128e9f128ab0a4b9c0485dd7d68bb5afff74
juuuun_b  <gudwns5220@gmail.com>
2021-06-26 13:56:32 +0900

    Update 44. 행성 터널.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/07. Seventh/08. 그래프 이론/44. 행성 터널.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/07. Seventh/08. 그래프 이론/44. 행성 터널.py
@@ -1,11 +1,57 @@
 from sys import stdin
 
 
-def solution():
-    return
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 
-print(solution())
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+def solution(n):
+    parent = [0] * (n + 1)
+    for i in range(1, n + 1):
+        parent[i] = i
+
+    edges, x, y, z = [], [], [], []
+
+    for i in range(1, n + 1):
+        a, b, c = map(int, stdin.readline().split())
+        x.append((a, i))
+        y.append((b, i))
+        z.append((c, i))
+
+    x.sort()
+    y.sort()
+    z.sort()
+
+    for i in range(n - 1):
+        edges.append((x[i + 1][0] - x[i][0], x[i + 1][1], x[i][1]))
+        edges.append((y[i + 1][0] - y[i][0], y[i + 1][1], y[i][1]))
+        edges.append((z[i + 1][0] - z[i][0], z[i + 1][1], z[i][1]))
+
+    edges.sort()
+    res = 0
+
+    for edge in edges:
+        cost, a, b = edge
+        if find_parent(parent, a) != find_parent(parent, b):
+            res += cost
+            union_parent(parent, a, b)
+
+    return res
+
+
+print(solution(int(stdin.readline())))
 
 """
 5
