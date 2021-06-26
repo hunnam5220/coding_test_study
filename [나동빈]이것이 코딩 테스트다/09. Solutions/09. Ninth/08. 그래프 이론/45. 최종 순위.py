ba04e705da4b7ff00cbf6f3fa0a8af4f6bd6b580
juuuun_b  <gudwns5220@gmail.com>
2021-06-26 14:58:23 +0900

    Update 45. 최종 순위.py

--- [나동빈]이것이 코딩 테스트다/09. Solutions/09. Ninth/08. 그래프 이론/45. 최종 순위.py
+++ [나동빈]이것이 코딩 테스트다/09. Solutions/09. Ninth/08. 그래프 이론/45. 최종 순위.py
@@ -1,11 +1,71 @@
 from sys import stdin
+from collections import deque
 
 
 def solution():
-    return
+    n = int(stdin.readline())
+    data = list(map(int, stdin.readline().split()))
+
+    arr = [[False] * (n + 1) for _ in range(n + 1)]
+    indegree = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        for j in range(i + 1, n + 1):
+            arr[data[i - 1]][data[j - 1]] = True
+            indegree[data[j - 1]] += 1
+
+    for _ in range(int(stdin.readline())):
+        a, b = map(int, stdin.readline().split())
+
+        if arr[a][b]:
+            arr[a][b] = False
+            arr[b][a] = True
+            indegree[b] -= 1
+            indegree[a] += 1
+        else:
+            arr[a][b] = True
+            arr[b][a] = False
+            indegree[b] += 1
+            indegree[a] -= 1
+
+    cy, res, cer = False, [], True
+    q = deque()
+
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    for _ in range(n):
+        if len(q) == 0:
+            cy = True
+            break
+
+        if len(q) >= 2:
+            cer = False
+
+        now = q.popleft()
+        res.append(now)
+
+        for j in range(1, n + 1):
+            if arr[now][j]:
+                indegree[j] -= 1
+                if indegree[j] == 0:
+                    q.append(j)
+
+    if cy:
+        print("IMPOSSIBLE")
+
+    elif not cer:
+        print("?")
+
+    else:
+        for i in res:
+            print(i, end=' ')
+        print()
 
 
-print(solution())
+for _ in range(int(stdin.readline())):
+    solution()
 
 """
 3
